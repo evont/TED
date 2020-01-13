1b700931b64e735ce17258c1cb74d9d56d18f3fa
evont <evontgoh@foxmail.com>
2020-01-13 23:54:53 +0800

    test

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,7 +2,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'Hello world';
+    this.ctx.body = '<html><title>Egg Nefelibata</title><body>Egg is growing</body></html>';
   }
 }
 
